43fbbd7eaf3f1153c0c80cf4b1deccd17ec6b5e7
Billan Abdi <billanabdi04@gmail.com>
2023-11-20 16:51:19 +0400

    sln

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function mostExpensive(product) {
+  let max = product[0];
+
+  for (let i = 0; i < items.length; i++) {
+    console.log(i);
+  }
+  if (product[0] > items) {
+    console.log("more expensive");
+  }
+}
+
+const items = [
+  { "Diamond Earrings": 980 },
+  { "Gold Watch": 250 },
+  { "{Pearl Necklace": 4650 },
+];
